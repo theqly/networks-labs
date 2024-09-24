13c69211afa13e611e9828e893b452d32ad40b6d
theqly <theqlgames@gmail.com>
2024-09-25 03:32:43 +0700

    trash

--- lab2/client/client.cpp
+++ lab2/client/client.cpp
@@ -2,13 +2,12 @@
 #include <arpa/inet.h>
 #include <iostream>
 #include <fstream>
-#include <memory>
 #include <sys/socket.h>
 #include <unistd.h>
 
 class client {
 private:
-    const uint16_t BUFFER_SIZE = 1024 * 1024;
+    const int BUFFER_SIZE = 1024;
 
     int sock = -1;
     const std::string& server_ip;
@@ -25,14 +24,16 @@ public:
 
         sockaddr_in server_sockaddr_in{};
         server_sockaddr_in.sin_family = AF_INET;
-        server_sockaddr_in.sin_port = htonl(server_port);
+        server_sockaddr_in.sin_port = htons(server_port);
         inet_pton(AF_INET, server_ip.c_str(), &server_sockaddr_in.sin_addr);
+
+        std::cout << server_ip.c_str() << std::endl;
 
         if(connect(sock, reinterpret_cast<sockaddr*>(&server_sockaddr_in), sizeof(server_sockaddr_in)) == -1) {
             throw std::runtime_error("error in connect()");
         }
 
-        std::cout << "Connected to " << server_ip << ":" << server_port << std::endl;
+        std::cout << "connected to " << server_ip << ":" << server_port << std::endl;
     }
 
     void send_file(const std::string& file_path) {
@@ -50,31 +51,73 @@ public:
         if(filename == nullptr) filename = file_path.c_str();
         else filename++;
 
+        std::cout << "opened a file and trying to send" << std::endl;
+
         // PROTOCOL:
         // 1. send filename
-        // 2. send size of file
-        // 3. send (size of file) bytes of file
+        // 2. recv response that filename received
 
-        if(send(sock, filename, strlen(filename), 0) == -1) {
+        // 3. send size of file
+        // 4. recv response that size of received
+
+        // 5. send (size of file) bytes of file
+        // 6. recv response that all (size of file) are received
+
+        char response[128];
+
+        if(send(sock, filename, strlen(filename) + 1, 0) == -1) {
             throw std::runtime_error("error in send(filename)");
         }
 
-        if(send(sock, &file_size, sizeof(file_size), 0) == -1) {
+        if(recv(sock, response, sizeof(response), 0) == -1) {
+            throw std::runtime_error("error in recv()");
+        }
+
+        std::cout << "sent a filename: " << filename << std::endl;
+
+        const uint64_t n_file_size = htonll(file_size);
+
+        if(send(sock, &n_file_size, sizeof(n_file_size), 0) == -1) {
             throw std::runtime_error("error in send(file size)");
         }
 
-        char buffer[BUFFER_SIZE];
-        uint64_t total_send = 0;
-        while(total_send < file_size) {
-            file.read(buffer, BUFFER_SIZE);
-            auto readed = file.gcount();
-
-            if(readed > 0) {
-                
-            }
+        if(recv(sock, response, sizeof(response), 0) == -1) {
+            throw std::runtime_error("error in recv()");
         }
 
+        std::cout << "sent a file size: " << file_size << std::endl;
 
+        char buffer[BUFFER_SIZE];
+        uint64_t sent_sum = 0;
+
+        while(sent_sum < file_size) {
+            file.read(buffer, BUFFER_SIZE);
+            auto read = file.gcount();
+
+            if(read > 0) {
+                ssize_t sent = send(sock, buffer, read, 0);
+                if(sent == -1){
+                    throw std::runtime_error("error in send()");
+                }
+                sent_sum += sent;
+            }
+
+            if(file.eof()) break;
+        }
+
+        std::cout << "after cycle" << std::endl;
+
+        file.close();
+
+        if(recv(sock, response, sizeof(response), 0) == -1){
+            throw std::runtime_error("error in recv()");
+        }
+
+        std::cout << "response from server: " << response << std::endl;
+    }
+
+    static uint64_t htonll(uint64_t value) {
+        return ((uint64_t)htonl(value & 0xFFFFFFFF) << 32) | htonl(value >> 32);
     }
 
     ~client() {
@@ -82,14 +125,28 @@ public:
             close(sock);
         }
     }
+
 };
 
 int main(int argc, char** argv) {
-    if(argc != 3) {
-        std::cout << "Usage: " << argv[0] << " <PATH TO FILE> <SERVER IP>" << std::endl;
+    if(argc != 4) {
+        std::cerr << "Usage: " << argv[0] << " <PATH TO FILE> <SERVER IP> <SERVER PORT>" << std::endl;
         return 1;
     }
 
+    std::string file_path = argv[1];
+    std::string server_ip = argv[2];
+    const int server_port = std::stoi(argv[3]);
+
+    client _client(server_ip, server_port);
+
+    try {
+        _client.connect_to_server();
+        _client.send_file(file_path);
+    } catch (const std::exception& e){
+        std::cerr << "catch exception: " << e.what() << std::endl;
+        return 1;
+    }
 
     return  0;
 }
--- lab2/server/server.cpp
+++ lab2/server/server.cpp
@@ -2,14 +2,21 @@
 #include <iostream>
 #include <sys/socket.h>
 #include <unistd.h>
+#include <thread>
+#include <fstream>
 
 class server {
 private:
+    const int MAX_FILE_PATH_LEN = 4096;
+    const int BUFFER_SIZE = 1024;
+
     int sock = -1;
     const int port;
 
+    int number_of_clients;
+
 public:
-    explicit server(const int port) : port(port) { }
+    explicit server(const int port) : port(port), number_of_clients(0) { }
 
     void run() {
         sock = socket(AF_INET, SOCK_STREAM, 0);
@@ -20,7 +27,7 @@ public:
 
         sockaddr_in server_sockaddr_in{};
         server_sockaddr_in.sin_family = AF_INET;
-        server_sockaddr_in.sin_port = htonl(port);
+        server_sockaddr_in.sin_port = htons(port);
         server_sockaddr_in.sin_addr.s_addr = INADDR_ANY;
 
         if(bind(sock, reinterpret_cast<sockaddr*>(&server_sockaddr_in), sizeof(server_sockaddr_in)) == -1) {
@@ -31,7 +38,7 @@ public:
             throw std::runtime_error("error in listen()");
         }
 
-        std::cout << "Server started on port " << port << std::endl;
+        std::cout << "server started on port " << port << std::endl;
 
         while(true) {
             sockaddr_in client_sockaddr_in{};
@@ -39,12 +46,19 @@ public:
             const int client_sock = accept(sock, reinterpret_cast<sockaddr*>(&client_sockaddr_in), &len);
 
             if (client_sock == -1) {
-                std::cout << "error in accept()" << std::endl;
+                std::cerr << "error in accept()" << std::endl;
                 continue;
             }
-            handle_client(client_sock);
+
+            std::thread(&server::handle_client, this, client_sock, number_of_clients).detach();
+
+            number_of_clients++;
         }
 
+    }
+
+    static uint64_t ntohll(uint64_t value) {
+        return ((uint64_t)ntohl(value & 0xFFFFFFFF) << 32) | ntohl(value >> 32);
     }
 
     ~server() {
@@ -54,8 +68,99 @@ public:
     }
 
 private:
-    void handle_client(int client_sock) {
-        std::cout << client_sock << std::endl;
+    void handle_client(const int client_sock, const int client_number) {
+
+        std::cout << "*** new connection ***" << std::endl;
+
+        char filename[MAX_FILE_PATH_LEN];
+
+        if(recv(client_sock, filename, sizeof(filename), 0) <= 0) {
+            std::cerr << "error in recv(filename)" << std::endl;
+            close(client_sock);
+            return;
+        }
+
+        std::cout << "received filename: " << filename << std::endl;
+
+        if(send(client_sock, "all right", sizeof("all right"), 0) == -1) {
+            throw std::runtime_error("error in send()");
+        }
+
+        uint64_t file_size;
+        if(recv(client_sock, &file_size, sizeof(file_size), 0) <= 0) {
+            std::cerr << "error in recv(file size)" << std::endl;
+            close(client_sock);
+            return;
+        }
+
+        file_size = ntohll(file_size);
+
+        std::cout << "received file size: " << file_size << std::endl;
+
+        if(send(client_sock, "all right", sizeof("all right"), 0) == -1) {
+            throw std::runtime_error("error in send()");
+        }
+
+        std::ofstream file("uploads/" + std::string(filename), std::ios::binary);
+        if(!file.is_open()) {
+            std::cerr << "error in opening file with path: " << "uploads/" + std::string(filename) << std::endl;
+            close(client_sock);
+            return;
+        }
+
+        char buffer[BUFFER_SIZE];
+        uint64_t received_sum = 0;
+
+        auto start_time = std::chrono::steady_clock::now();
+        auto last_time = std::chrono::steady_clock::now();
+
+        while(received_sum < file_size) {
+
+            ssize_t received = recv(client_sock, buffer, sizeof(buffer), 0);
+
+            if(received <= 0) break;
+
+            file.write(buffer, received);
+            received_sum += received;
+
+            auto current_time = std::chrono::steady_clock::now();
+
+            std::chrono::duration<double> brk = current_time - last_time;
+
+            if(brk.count() >= 3.0) {
+
+                std::chrono::duration<double> general_brk = current_time - start_time;
+
+                double instant_speed = ((double)received / 1024.0) / brk.count();
+                double average_speed = ((double)received_sum / 1024.0) / general_brk.count();
+
+                std::cout << "*** client " << client_number << " *** " << "instant speed: " << instant_speed << " kb/s, average speed: " << average_speed << " kb/s"<< std::endl;
+
+                last_time = current_time;
+            }
+        }
+
+        auto overall_time = std::chrono::duration<double>(std::chrono::steady_clock::now() - start_time);
+
+        if(overall_time.count() < 3.0){
+            std::cout << "*** client " << client_number << " *** " << "speed: " << ((double)received_sum / 1024.0) / overall_time.count() << " kb/s" << std::endl;
+        }
+
+        std::cout << "after cycle" << std::endl;
+
+        file.close();
+
+        if(received_sum == file_size) {
+            if(send(client_sock, "all right", sizeof("all right"), 0) == -1) {
+                throw std::runtime_error("error in send()");
+            }
+        } else {
+            if(send(client_sock, "all bad", sizeof("all bad"), 0) == -1) {
+                throw std::runtime_error("error in send()");
+            }
+        }
+
+        close(client_sock);
     }
 };
 
@@ -63,8 +168,18 @@ private:
 
 int main(int argc, char** argv) {
     if(argc != 2) {
-        std::cout << "Usage: " << argv[0] << " <PORT>" << std::endl;
+        std::cerr << "Usage: " << argv[0] << " <PORT>" << std::endl;
         return 1;
+    }
+
+    const int port = std::stoi(argv[1]);
+
+    server _server(port);
+
+    try{
+        _server.run();
+    } catch (const std::exception& e) {
+        std::cerr << "catch exception: " << e.what() << std::endl;
     }
 
 
